780b315bc14613845bb029e5218b417206c3fb1f
QuentinMaignan <quentindu4913@hotmail.fr>
2021-12-03 00:37:20 +0100

    part 2

--- Day1/SonarSweep.py
+++ Day1/SonarSweep.py
@@ -1,18 +1,15 @@
 # --- Day 1: Sonar Sweep ---
 text_file = open("input.txt", "r")
-depths_report = text_file.readlines()
+depths_report = (list(map(int, text_file.readlines())))
 text_file.close()
 
-measurement_increases = 0
-
-for index, depth in enumerate(depths_report):
-    if index == 0:
-        # print(depth, ", (N/A - no previous measurement)")
-        continue
-    if depth > depths_report[index - 1]:
-        measurement_increases += 1
-        # print(depth, ", (increased)")
-    # else:
-        # print(depth, ", (decreased)")
+measurement_increases = sum(depths_report[i] > depths_report[i - 1] for i in range(1, len(depths_report)))
+measurement_increases_part_2 = 0
+for i in range(len(depths_report) - 3):
+    A = depths_report[i] + depths_report[i+1] + depths_report[i+2]
+    B = depths_report[i+1] + depths_report[i+2] + depths_report[i+3]
+    if A < B:
+        measurement_increases_part_2 += 1
 
 print("number of times depth was increasing : ", measurement_increases)
+print("number of times depth was increasing part 2: ", measurement_increases_part_2)
